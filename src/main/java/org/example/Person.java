77c4c03c46dd0398bc3cb6e3836a0c87c6185551
Adrian Garett Sian <adriansian@gmail.com>
2025-07-11 13:43:30 +0800

    Adds Visitor and Person classes
    Introduces base Person class with name, location, contact info.
    
    Creates Visitor class extending Person, including age, ticket details, and logic to determine ticket type/price.
    
    Signed-off-by: Adrian Garett Sian

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -7,10 +7,32 @@ public abstract class Person {
     protected String contactNumber;
     protected String email;
 
-    public Person(String name, Building location, String contactNumber, String email) {
+    public Person (String name){
         this.name = name;
-        this.location = location;
-        this.contactNumber = contactNumber;
-        this.email = email;
+        this.location = null; // Initially, the person has no location
+        this.contactNumber = "";
+        this.email = "";
     }
+    public void goTo(Building destination){
+        System.out.println(name + " is going to " + destination);
+        this.location = destination;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public Building getLocation() {
+        return location;
+    }
+    public String getContactNumber() {
+        return contactNumber;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
