6f515a0ea29902b20443f23bc9ba552906a78c46
Ivan Patrick Frondozo <ivan-p.frondozo@GXCHLAP00704.local>
2025-07-13 19:10:35 +0800

    Added VeterinarianModule, updated Veterinarian, Building, and Hospital class for functionality of the module

--- src/main/java/org/example/People/Veterinarian.java
+++ src/main/java/org/example/People/Veterinarian.java
@@ -1,12 +1,21 @@
 package org.example.People;
 
+import org.example.Building;
 import org.example.Person;
 
 public class Veterinarian extends Person {
 
     public Veterinarian(String name) {
         super(name);
-        this.location = new Building ();
+        this.location = new Building();
+    }
+
+    public void lecture(){
+        System.out.println("Please treat animals well even it is not your pet. Animals must be loved and protected!");
+    }
+
+    public void heal(){
+        System.out.println("All animals are healed!");
     }
 
 }
